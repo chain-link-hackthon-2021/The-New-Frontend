73a4ac0c441a5c1a4d228d97361f86b129e89558
Beture20 <femigbajuola@gmail.com>
2022-05-03 12:38:00 +0100

    3.3.6  commit

--- app/Views/products/editProduct.php
+++ app/Views/products/editProduct.php
@@ -3,15 +3,15 @@
 <?= $this->section('content') ?>
 
 <?php if (session()->getFlashdata('success')) : ?>
-    <div class="alert alert-success">
-        <?= session()->getFlashdata('success'); ?>
-    </div>
+<div class="alert alert-success">
+    <?= session()->getFlashdata('success'); ?>
+</div>
 <?php endif; ?>
 
 <?php if (session()->getFlashdata('error')) : ?>
-    <div class="alert alert-danger">
-        <?= session()->getFlashdata('error'); ?>
-    </div>
+<div class="alert alert-danger">
+    <?= session()->getFlashdata('error'); ?>
+</div>
 <?php endif; ?>
 
 <!-- input style start -->
@@ -25,64 +25,36 @@
                 <div class="col-md-5">
                     <div class="input-style-1">
                         <label for="ProductName">Name</label>
-                        <input type"text" id="ProductName" maxlength="58" name="ProductName" disabled value="<?= $products['product'][0]['productName'] ?>" required />
+                        <input type"text" id="ProductName" maxlength="58" name="ProductName" disabled
+                            value="<?= $products['product'][0]['productName'] ?>" required />
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="input-style-1">
                         <label for="ProductName">Stock count</label>
-                        <input type"text" id="stock" maxlength="58" name="stock" value="<?= $products['product'][0]['stock'] ?>" required />
+                        <input type"text" id="stock" maxlength="58" name="stock"
+                            value="<?= $products['product'][0]['stock'] ?>" required />
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="input-style-1">
                         <label for="ProductPrice">Price (<?= $shops[0]['CurrencyType']; ?>) </label>
-                        <input type="text" id="ProductPrice" maxlength="58" name="ProductPrice" value="<?= $products['product'][0]['productPrice'] ?>" placeholder="0.0" required />
+                        <input type="text" id="ProductPrice" maxlength="58" name="ProductPrice"
+                            value="<?= $products['product'][0]['productPrice'] ?>" placeholder="0.0" required />
                     </div>
                 </div>
                 <div class="col-md-12">
                     <div class="input-style-1">
                         <label for="description">Description</label>
-                        <textarea name="description" id="description" cols="30" rows="10"><?= $products['product'][0]['description'] ?></textarea>
+                        <textarea name="description" id="description" cols="30"
+                            rows="10"><?= $products['product'][0]['description'] ?></textarea>
                     </div>
                 </div>
                 <div class="col-md-12">
                     <div class="input-style-1">
                         <label for="ProductType">Product Type</label>
-                        <input type="text" id="ProductType" maxlength="58" name="ProductType" value="<?= $products['product'][0]['productType'] ?>" />
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <h6 class="mb-3">
-                        Gateways <i class="fa fa-question-circle" rel="tooltip" title="Please note: you must have these options enabled and set up in the shop settings and enabled here to see it as a payment option."></i>
-                    </h6>
-                    <div class="row">
-                        <div class="col text-center">
-                            <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
-                                <input class="custom-control-input" id="sw-stripe" type="checkbox" <?php if ($products['product'][0]['stripeEnabled'] == "true") {
-                                                                                                        echo "checked='checked'";
-                                                                                                    } ?> name="StripeEnabled" value="true">
-                                <label class="custom-control-label" for="sw-stripe">Stripe</label>
-                            </div>
-                        </div>
-
-                        <div class="col text-center">
-                            <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
-                                <input class="custom-control-input" id="sw-coinbase-commerce" <?php if ($products['product'][0]['coinbaseCommerceEnabled'] == "true") {
-                                                                                                    echo "checked='checked'";
-                                                                                                } ?> type="checkbox" data-val="true" name="CoinbaseCommerceEnabled" value="true">
-                                <label class="custom-control-label" for="sw-coinbase-commerce">Cryptocurrency</label>
-                            </div>
-                        </div>
-
-                        <div class="col text-center">
-                            <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
-                                <input class="custom-control-input" id="sw-paypal" <?php if ($products['product'][0]['payPalEnabled'] == "true") {
-                                                                                        echo "checked='checked'";
-                                                                                    } ?> type="checkbox" data-val="true" data-val-required="The PayPal field is required." name="PayPalEnabled" value="true">
-                                <label class="custom-control-label" for="sw-paypal">PayPal</label>
-                            </div>
-                        </div>
+                        <input type="text" id="ProductType" maxlength="58" name="ProductType"
+                            value="<?= $products['product'][0]['productType'] ?>" />
                     </div>
                 </div>
             </div>
@@ -94,34 +66,44 @@
                     <hr>
                     <div class="text-center bg-gray-light">
                         <input type="hidden" name="oldImage" value="<?= $products['product'][0]['productImage']; ?>">
-                        <img style="height:200px;" id="blah" class="img-fluid" src="<?= $products['product'][0]['productImage']; ?>" alt="your image">
+                        <img style="height:200px;" id="blah" class="img-fluid"
+                            src="<?= $products['product'][0]['productImage']; ?>" alt="your image">
                     </div>
                     <div class="input-style-1">
                         <input type="file" accept="image/*" id="productImage" name="productImage" />
                     </div>
                 </div>
-                <div class="col-md-12" style="background-color: #fff; padding: 15px; border-radius: 12px; margin-bottom: 20px">
+                <div class="col-md-12"
+                    style="background-color: #fff; padding: 15px; border-radius: 12px; margin-bottom: 20px">
                     <h5 class="mb-25">Product Options</h5>
                     <div class="form-group col-md-12">
                         <label class="control-label" for="Position">Position</label>
-                        <input class="form-control" type="number" data-val="true" data-val-required="The Position field is required." id="Position" name="Position" value="100000" />
+                        <input class="form-control" type="number" data-val="true"
+                            data-val-required="The Position field is required." id="Position" name="Position"
+                            value="100000" />
                     </div>
                     <div class="form-group col-md-12">
                         <label class="control-label" for="WebhookUrl">Webhook URL</label>
-                        <input class="form-control" type="text" id="WebhookUrl" value="<?= $products['product'][0]['webhookUrl'] ?>" name="WebhookUrl" placeholder="Discord hooks supported!" value="" />
+                        <input class="form-control" type="text" id="WebhookUrl"
+                            value="<?= $products['product'][0]['webhookUrl'] ?>" name="WebhookUrl"
+                            placeholder="Discord hooks supported!" value="" />
                     </div>
                     <div class="form-group col-md-12">
                         <label class="control-label" for="CallbackURL">Callback URL</label>
-                        <input class="form-control" type="text" id="CallbackURL" value="<?= $products['product'][0]['callbackURL'] ?>" maxlength="100" name="CallbackURL" value="" />
+                        <input class="form-control" type="text" id="CallbackURL"
+                            value="<?= $products['product'][0]['callbackURL'] ?>" maxlength="100" name="CallbackURL"
+                            value="" />
                     </div> <br><br>
                     <div class="row">
                         <div class="col">
                             <label for="sw-stripe-google-pay">Unlisted</label>
-                            <i class="fa fa-question-circle" rel="tooltip" title="This product won't show on your shop page"></i>
+                            <i class="fa fa-question-circle" rel="tooltip"
+                                title="This product won't show on your shop page"></i>
                         </div>
                         <div class="col-auto">
                             <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="sw-unlisted" data-val="true" data-val-required="The Unlisted field is required." name="Unlisted" value="true">
+                                <input type="checkbox" class="custom-control-input" id="sw-unlisted" data-val="true"
+                                    data-val-required="The Unlisted field is required." name="Unlisted" value="true">
                                 <label class="custom-control-label" for="sw-unlisted"></label>
                             </div>
                         </div>
@@ -132,7 +114,9 @@
                         </div>
                         <div class="col-auto">
                             <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="sw-blockproxy" data-val="true" data-val-required="The Block VPNs/Proxies field is required." name="BlockProxy" value="true">
+                                <input type="checkbox" class="custom-control-input" id="sw-blockproxy" data-val="true"
+                                    data-val-required="The Block VPNs/Proxies field is required." name="BlockProxy"
+                                    value="true">
                                 <label class="custom-control-label" for="sw-blockproxy"></label>
                             </div>
                         </div>
--- app/Views/products/products.php
+++ app/Views/products/products.php
@@ -6,14 +6,14 @@
     <div class="col-lg-12">
         <div class="card-style mb-30">
             <div class="title d-flex flex-wrap justify-content-between">
-                  <div class="left">
+                <div class="left">
                     <h4 class="text-bold">Products</h4>
-                  </div>
-                  <div class="right">
+                </div>
+                <div class="right">
                     <a href="/Shop/<?= $name; ?>/ProductCreate" class="btn btn-primary"> New Product</a>
                     <!-- end select -->
-                  </div>
                 </div>
+            </div>
             <div class="table-wrapper table-responsive">
                 <table class="table">
                     <thead>
@@ -22,7 +22,7 @@
                                 <h6 style="font-weight: 900; ">Product Name</h6>
                             </th>
                             <th>
-                                <h6 style="font-weight: 900; ">Price (<?= $shops[0]['CurrencyType']; ?>)</h6>
+                                <h6 style="font-weight: 900; ">Price </h6>
                             </th>
                             <th>
                                 <h6 style="font-weight: 900; ">Stock</h6>
@@ -34,52 +34,55 @@
                         <!-- end table row-->
                     </thead>
                     <tbody>
-                        <?php if(empty($products)){
+                        <?php if (empty($products)) {
+                        ?>
+                        <tr class="odd">
+                            <td colspan="5" class="min-width" valign="top">No data available in table</td>
+                        </tr>
+                        <?php
+                        } else {
+                            $ii = 0;
+                            foreach ($products as $product) {
+                                $ii++;
+                                $color = ($ii % 2 !== 0) ? "#b0c4de" : "#efefef";
                             ?>
-                                <tr class="odd">
-                                    <td colspan="5" class="min-width" valign="top">No data available in table</td>
-                                </tr>
-                            <?php
-                            } else {
-                                $ii = 0;
-                                foreach($products as $product){
-                                    $ii++;
-                                    $color = ($ii % 2 !== 0) ? "#b0c4de" : "#efefef";
-                                    ?>
-                                        <tr style="background-color: <?= $color ?>; border-top: 2px solid black;">
-                                            <td class="min-width">
-                                                <a href="/@<?= $name; ?>/Product/<?= $product['uniqueID']; ?>">
-                                                    <?= $product['productName']; ?>
-                                                </a>
-                                                <div class="text-muted small d-none d-lg-block" style="font-size:.75em;">
-                                                    <?= $product['uniqueID']; ?>
-                                                </div>
-                                            </td>
-                                            <td class="min-width">
-                                                <?= $product['productPrice']; ?>
-                                            </td>
-                                            <td class="min-width">
-                                                <?php
-                                                    if($product['stock'] < 10){
-                                                        $stockClass = "text-danger";
-                                                    } else {
-                                                        $stockClass = "text-primary";
-                                                    }
-                                                ?>
-                                                <span class="<?= $stockClass; ?>"> <?= $product['stock']; ?></span>
-                                            </td>
-                                            <td class="min-width">
-                                                <div id="btn-controls">
-                                                    <a class="btn" title="Edit product" id="editbtn" href="/Shop/<?= $name; ?>/ProductEdit/<?= $product['uniqueID']; ?>">
-                                                        <em class="lni lni-pencil text-primary" id="edit" ></em>
-                                                    </a>
-                                                    <button id="delbtn" class="btn" onclick="if (!window.__cfRLUnblockHandlers) return false; deleteProduct('<?= $product['uniqueID']; ?>', 'E-commerce website')" ><em class="lni lni-trash-can" style="color: red;" id="delete"></em></button>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    <?php
-                                }
+                        <tr style="background-color: <?= $color ?>; border-top: 2px solid black;">
+                            <td class="min-width">
+                                <a href="/@<?= $name; ?>/Product/<?= $product['uniqueID']; ?>">
+                                    <?= $product['productName']; ?>
+                                </a>
+                                <div class="text-muted small d-none d-lg-block" style="font-size:.75em;">
+                                    <?= $product['uniqueID']; ?>
+                                </div>
+                            </td>
+                            <td class="min-width">
+                                <?= $product['productPrice']; ?>
+                            </td>
+                            <td class="min-width">
+                                <?php
+                                        if ($product['stock'] < 10) {
+                                            $stockClass = "text-danger";
+                                        } else {
+                                            $stockClass = "text-primary";
+                                        }
+                                        ?>
+                                <span class="<?= $stockClass; ?>"> <?= $product['stock']; ?></span>
+                            </td>
+                            <td class="min-width">
+                                <div id="btn-controls">
+                                    <a class="btn" title="Edit product" id="editbtn"
+                                        href="/Shop/<?= $name; ?>/ProductEdit/<?= $product['uniqueID']; ?>">
+                                        <em class="lni lni-pencil text-primary" id="edit"></em>
+                                    </a>
+                                    <button id="delbtn" class="btn"
+                                        onclick="deleteFunction('<?= $product['uniqueID']; ?>')"><em
+                                            class="lni lni-trash-can" style="color: red;" id="delete"></em></button>
+                                </div>
+                            </td>
+                        </tr>
+                        <?php
                             }
+                        }
                         ?>
                     </tbody>
                 </table>
@@ -91,5 +94,70 @@
     <!-- end col -->
 </div>
 <!-- end row -->
+<script>
+function deleteFunction(id) {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: true
+    })
 
+    swalWithBootstrapButtons.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true
+    }).then(async (result) => {
+        if (result.isConfirmed) {
+            try {
+                const res = await axios.post("<?= getenv("soapBaseUrl") ?>api/v1/product/deleteById", JSON
+                    .stringify({
+                        "productId": id
+                    }), {
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Headers": "*",
+                            "Content-Type": "multipart/form-data",
+                            "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+                            "content-type": "application/json",
+                            "Access-Control-Allow-Methods": " GET, POST, PUT, DELETE",
+                        },
+                    });
+                if (res.data.status == "success") {
+                    swalWithBootstrapButtons.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    )
+                    window.location.href = "";
+                } else {
+                    swalWithBootstrapButtons.fire(
+                        'Deleted!',
+                        'Your file has been ....',
+                        'error'
+                    )
+                }
+            } catch (error) {
+
+            }
+
+
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swalWithBootstrapButtons.fire(
+                'Cancelled',
+                'Your  file is safe :)',
+                'error'
+            )
+        }
+    })
+}
+</script>
 <?= $this->endSection() ?>
